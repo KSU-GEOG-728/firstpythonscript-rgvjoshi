ee3da4ae711cd6bdf4758aa48d13049653e2ea95
Shawn Hutchinson <shutch@ksu.edu>
2024-09-18 08:14:46 -0500

    Instructor Corrections
    Original script ran to completion and generated correct results.
    
    There is no requirement to import "arcpy.analysis" and "arcpy.management".  I also removed the use of one result object.  This doesn't break your code, but is not needed or helpful...yet.

--- GitHub-FirstPythonScript.py
+++ GitHub-FirstPythonScript.py
@@ -11,8 +11,6 @@
 
 # Import arcpy module and allow overwrites
 import arcpy
-import arcpy.analysis
-import arcpy.management
 arcpy.env.overwriteOutput = True
 
 # Set current workspace
@@ -22,10 +20,10 @@ arcpy.env.workspace = "C:\\Users\\rgvjoshi\\OneDrive - Kansas State University\\
 flint = arcpy.management.SelectLayerByAttribute('ks_ecoregions', 'NEW_SELECTION', "US_L3NAME = 'Flint Hills'")
 
 #Create a buffer of 10 kilometers around Flint Hilld
-buffer = arcpy.analysis.Buffer(flint, 'Buff_Flint_ecoregion', '10 kilometers')
+arcpy.analysis.Buffer(flint, 'Buff_Flint_ecoregion', '10 kilometers')
 
 #Clip rivers inside this buffer
-clipped = arcpy.analysis.Clip('ks_major_rivers', buffer,'Rivers_in_FH_clip')
+clipped = arcpy.analysis.Clip('ks_major_rivers', 'Buff_Flint_ecoregion', 'Rivers_in_FH_clip')
 
 # Calculate length of streams inside Flint Hills ecoregion in miles
 arcpy.management.CalculateGeometryAttributes(clipped, [['StreamLength_miles', 'LENGTH']], 'MILES_INT')
